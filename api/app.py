4ce4f51dca4c49aa547cb05ed7470c8f7346e22e
Ziyang Jiao <ziyang.jiao23@imperial.ac.uk>
2023-10-23 19:08:57 +0100

    10.23 with tests

--- api/app.py
+++ api/app.py
@@ -12,3 +12,14 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+
+@app.route("/query")
+def process_query(query):
+	#query = request.args.get('q', 'dinosaurs')
+	if query == "dinosaurs":
+		return "Dinosaurs ruled the Earth 200 million years ago"
+	elif query == "asteroids":
+		return "Unknown"
+	else:
+		return f"You searched for: {query}"
