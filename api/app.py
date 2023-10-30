727d7298a68ee61dc10367291e4c45ac3552f7f9
Ziyang Jiao <ziyang.jiao23@imperial.ac.uk>
2023-10-30 19:53:24 +0000

    s8

--- api/app.py
+++ api/app.py
@@ -25,6 +25,8 @@ def process_query(query):
     add_match = re.search(r"What is (\d+) plus (\d+)?", query, re.I)
     mul_match = re.search(r"What is (\d+) multiplied by (\d+)?", query, re.I)
     minus_match = re.search(r"What is (\d+) minus (\d+)?", query, re.I)
+    prime_match = re.search(r"Which of the following numbers\
+            are primes: (\d+), (\d+), (\d+), (\d+), (\d+)?", query, re.I)
     if query == "dinosaurs":
         return "Dinosaurs ruled the Earth 200 million years ago"
     elif query == "asteroids":
@@ -38,7 +40,20 @@ def process_query(query):
     elif minus_match:
         num1, num2 = map(int, minus_match.groups())
         return str(num1 - num2)
+    elif prime_match:
+        num1, num2, num3, num4, num5 = map(int, minus_match.groups())
+        return is_prime(num1) + is_prime(num2) + is_prime(num3) + \
+            is_prime(num4) + is_prime(num5)
     elif query == "What is your name?":
         return "VW50"
     else:
         return f"You searched for: {query}"
+
+
+def is_prime(n):
+    if n < 2:
+        return ""
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return ""
+    return str(n)
